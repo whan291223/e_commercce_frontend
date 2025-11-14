a01af8e2efda0b006fb9df53d3bc837610cffc8a
jedai <jedai2543@gmail.com>
2025-11-14 23:53:23 +0700

    admindashboard - 2

--- src/components/admin/AdminDashboard.jsx
+++ src/components/admin/AdminDashboard.jsx
@@ -5,4 +5,24 @@ import {fetchCategories, fetchProducts} from '../../api/CategoryApi'
 import AddCategoryModal from './AddCategoryModel'
 import AddProductModal from "./AddProductModel";
 
-function Admin
+function AdminDashboard() {
+    const [categoryCount, setCategoryCount] = useState(0);
+    const [productCount, setProductCount] = useState(0);
+    const [isCategoryModalOpen, setCategoryModalOpen] = useState(false);
+    const [isProductModalOpen, setProductModalOpen] = useState(false);
+
+    const updateCounts = useCallback( async () => {
+        try {
+            const catRes = await fetchCategory();
+            const prodRes = await fetchAllProduct();
+            setCategoryCount(catRes.data.length);
+            setProductCount(prodRes.data.length); // confuse why use.data.length
+
+        } catch (error) {
+            console.error("Failed to update counts:", error)
+        }
+    }, []);
+
+    useEffect
+
+}
